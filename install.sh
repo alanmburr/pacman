b8a3ec03d2098c64d523cb85a2ac6a4b0923fae6
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2020-06-24 07:18:29 -0400

    fixed install.sh

--- install.sh
+++ install.sh
@@ -5,31 +5,21 @@ sh startup.sh
 allpackage ()
 {
 	#chrome install
-	cd /lib/linstall86/bin
+	cd /lib/linstall86
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 	apt install ./google-chrome-stable_current_amd64.deb
 	google-chrome
 	#VLC install
-	cd /lib/linstall86/bin
-	wget http://archive.ubuntu.com/ubuntu/pool/universe/v/vlc/vlc_3.0.11-1_amd64.deb
-	apt install vlc_3.0.11-1_amd64.deb
-	#VS code & nanoinstall
-	cd /lib/linstall86/bin
-	apt install software-properties-common apt-transport-https wget
-	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-	add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-	apt update
-	apt install code
-	apt install nano
-	apt install vi
+	sudo snap VLC
+	#VS code install
+	sudo snap install --classic code
 }
-rboot () {shutdown -r now}
 while true; do
-	read -p "Installation finished. Would you like to restart now? (y,n)" ynx
+	read -p "Do you want to install all packages? (y,n)" ynx
 	case $ynx in 
-		[Yy]* ) rboot; break;;
-		[Nn]* ) ;;
-        [Xx]* ) exit; exit; exit;;
+		[Yy]* ) allpackage; break;;
+		[Nn]* ) sh bin/chrome.sh;;
+		[Xx]* ) exit; exit;;
 		* ) "plz type 'y', 'Y', 'n' or 'N'. Thx."
 	esac
 done
