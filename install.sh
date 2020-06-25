2f1cf4252d2badb1fcf41dd7ad575187d9c14d96
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2020-06-25 14:27:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,12 +14,22 @@ allpackage ()
 	snap install vlc
 	#VS code & nanoinstall
 	cd /lib/linstall86/bin
+	rm -r /var/lib/dpkg/lock
+	rm -r /var/lib/dpkg/lock-frontend
 	apt install software-properties-common apt-transport-https wget
 	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 	add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+	rm -r /var/lib/dpkg/lock
+	rm -r /var/lib/dpkg/lock-frontend
 	apt update
+	rm -r /var/lib/dpkg/lock
+	rm -r /var/lib/dpkg/lock-frontend
 	apt install code
+	rm -r /var/lib/dpkg/lock
+	rm -r /var/lib/dpkg/lock-frontend
 	apt install nano
+	rm -r /var/lib/dpkg/lock
+	rm -r /var/lib/dpkg/lock-frontend
 	apt install vi
 	echo "Also installed nano and vi. yk, jic."
 }
