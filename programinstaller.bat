4ca151ae61b8303d340727fd3b512865e91c2673
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2020-07-05 18:32:45 -0400

    Add files via upload

---
+++ programinstaller.bat
@@ -0,0 +1,63 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------   
+powershell 
+$Output = $wshelll.Popup("The installer will now begin installing programs. Please stay at your computer in case there is a popup.",0,"Software Installation",0+64)
+$Output = $wshell.Popup("Installing Google Chrome Stable for Windows",5,"Software Install",0+64)
+mkdir c:\"program files"\Winstall64
+cd ..
+xcopy programinstall c:\"program files"\Winstall64
+$Output = $wshell.Popup("Installing Lego Mindstorms",0,"Software Installation",0+64)
+cd programinstall
+start ./LMS-EV3_Full-setup_1.4.4_en-US_WIN32.exe
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+cmd
+@echo off
+SET DIR=%~dp0%
+::download install.ps1
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1','%DIR%install.ps1'))"
+::run installer
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%install.ps1' %*"
+cmd
+powershell
+$Output = $wshell.Popup("Installing other software",0,"Software Installation",0+64)
+choco install irfanview -y
+choco install firefox -y
+choco install 7zip.install -y
+choco install vlc -y
+choco install python -y
+choco install libreoffice-still -y
+choco install adobereader -y
+choco install vscode -y
+choco install googlechrome -y
+$Output = $wshell.Popup("Install Finished. Your computer will restart soon. Be sure to boot into Windows.",0,"Software Installation",0+48)
+timeout /t 2000 /nobreak
+Restart-Computer
+exit
