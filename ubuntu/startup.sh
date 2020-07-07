b0b06f6ec5ec51be0b29a9b3229f2c69ebc3c63c
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2020-07-06 21:02:21 -0400

    Update startup.sh
    Delete install files for macOS and Windows.

--- ubuntu/startup.sh
+++ ubuntu/startup.sh
@@ -1,24 +1,15 @@
 if [ "$(id -u)" != "0" ]; then
   exec sudo "sh" "startup.sh"
 fi
+cd ..
+rm -r windows
+rm -r mac
 echo "Using mkdir to make a directory in '/lib'. The directory will be named 'linstall86'."
 mkdir /lib/linstall86
-#ls /lib/linstall86
 cp install.sh /lib/lnstall86/install.sh
 cp startup.sh /lib/linstll86/startup.sh
 cd /lib/linstall86
 mkdir /lib/linstall86/bin
-#rm bin
-#echo "Removing 'bin' as it s already here."
 clear
 echo "Packages to install: VLC, Visual Studio Code, Google Chrome. Packages already installed: Firefox, Python, LibreOffice, Chrome PDF Viewer"
 echo "$(tput setaf 1)Attention: $(tput setab 7)Your computer will restart after this installation. If you are dual-booting, be sure to boot back into this OS.$(tput sgr 0)"
-#while true; do
-#	read -p "Do you want to restart directly after the installation? (y,n)" ynx
-#	case $ynx in 
-#		[Yy]* ) ; break;;
-#		[Nn]* ) vlcinstall;;
- #   [Xx]* ) exit; exit; exit;;
-	#	* ) "plz type 'y', 'Y', 'n' or 'N'. Thx."
-#	esac
-#done
