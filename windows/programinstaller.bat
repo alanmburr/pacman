ebdadab99c994a7db364efa34fe25e33bbf825f7
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2021-01-26 19:44:37 -0500

    Update programinstaller.bat

--- windows/programinstaller.bat
+++ windows/programinstaller.bat
@@ -1,5 +1,21 @@
 @echo off
 
+rem    Copyright (C) 2021 Alan Burr <alanburr25@gmail.com>
+    
+rem    This program is free software: you can redistribute it and/or modify
+rem    it under the terms of the GNU General Public License as published by
+rem    the Free Software Foundation, either version 3 of the License, or
+rem    (at your option) any later version.
+
+rem    This program is distributed in the hope that it will be useful,
+rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem    GNU General Public License for more details.
+
+rem    You should have received a copy of the GNU General Public License
+rem    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 :: BatchGotAdmin
 :-------------------------------------
 REM  --> Check for permissions
